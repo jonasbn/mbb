74bc5ca2ee33202284d3b5ae8deaa95713c09bc3
jonasbn <jonasbn@logiclab.dk>
2011-05-06 15:53:06 +0000

    Prepping release
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/MBB/trunk@7663 e6edf6fb-f266-4316-afb4-e53d95876a76

--- lib/Module/Build/Bundle.pm
+++ lib/Module/Build/Bundle.pm
@@ -14,7 +14,7 @@ use base qw(Module::Build);
 
 use constant EXTENDED_POD_LINK_VERSION => 5.12.0;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 #HACK: we need a writable copy for testing purposes
 our $myPERL_VERSION = $^V; 
@@ -202,7 +202,7 @@ Module::Build::Bundle - subclass for supporting Tasks and Bundles
 
 =head1 VERSION
 
-This documentation describes version 0.01
+This documentation describes version 0.06
 
 =head1 SYNOPSIS
 
