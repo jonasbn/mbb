d1b093f303e5380c98fb371d9973b1ec1882d144
jonasbn <jonasbn@logiclab.dk>
2012-11-25 20:29:46 +0000

    Addressed Perl::Critic::Policy Variables::ProhibitPackageVars
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/MBB/trunk@7987 e6edf6fb-f266-4316-afb4-e53d95876a76

--- lib/Module/Build/Bundle.pm
+++ lib/Module/Build/Bundle.pm
@@ -17,7 +17,7 @@ use constant EXTENDED_POD_LINK_VERSION => 5.12.0;
 our $VERSION = '0.08';
 
 #HACK: we need a writable copy for testing purposes
-## no critic (Variables::ProhibitPackageVars9
+## no critic qw(Variables::ProhibitPackageVars)
 our $myPERL_VERSION = $^V;
 
 sub ACTION_build {
