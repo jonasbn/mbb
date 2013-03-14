9b6cab6d31357842a3dd1cc13e383e595b8e9112
jonasbn <jonasbn@logiclab.dk>
2013-03-14 22:06:50 +0000

    Updated copyright notice
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/MBB/trunk@8083 e6edf6fb-f266-4316-afb4-e53d95876a76

--- lib/Module/Build/Bundle.pm
+++ lib/Module/Build/Bundle.pm
@@ -537,7 +537,7 @@ but then I needed it for some other distributions, so I decided to separate it o
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2010-2012 jonasbn, all rights reserved.
+Copyright 2010-2013 jonasbn, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
