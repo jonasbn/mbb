e0100b347682df1ddb17b9a59fec81182abca45c
jonasbn <jonasbn@logiclab.dk>
2013-03-15 07:39:40 +0000

    Ran code though Perl::Tidy one more time
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/MBB/trunk@8094 e6edf6fb-f266-4316-afb4-e53d95876a76

--- lib/Module/Build/Bundle.pm
+++ lib/Module/Build/Bundle.pm
@@ -14,9 +14,9 @@ use base qw(Module::Build);
 use utf8;
 
 use constant EXTENDED_POD_LINK_VERSION => 5.12.0;
-use constant PERMISSION_MASK => '07777';
-use constant WRITEPERMISSION => '0644';
-use constant FILEMODE => 2;
+use constant PERMISSION_MASK           => '07777';
+use constant WRITEPERMISSION           => '0644';
+use constant FILEMODE                  => 2;
 
 our $VERSION = '0.11';
 
@@ -94,7 +94,8 @@ sub ACTION_contents {
 
     ## no critic (Bangs::ProhibitBitwiseOperators)
     my $permissions = ( stat $file )[FILEMODE] & PERMISSION_MASK;
-    chmod WRITEPERMISSION, $file or croak "Unable to make file: $file writable - $!";
+    chmod WRITEPERMISSION, $file
+        or croak "Unable to make file: $file writable - $!";
 
     open my $fout, '>', $file
         or croak "Unable to open file: $file - $!";
